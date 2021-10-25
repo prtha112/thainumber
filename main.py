c91a429288636df57eeefb5af8ab9d0ab66a25d2
Sathabhron <thahi5_vi@hotmail.com>
2021-10-25 15:14:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,40 @@ def toThaiNumber(input_number):
     unit = ("", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน")
     number = input_number
     i = len(number)
+    f = len(number) - 1
+    tmp_number = ""
     result = ""
+    print(f)
     for c in number:
         i -= 1
-        print(i)
         if i > 6:
             x = i - 6
         else:
             x = i
         unit_part = unit[x];
-        if c == '1' and x == 0:
+        if c == '1' and x == 0 and tmp_number != '0':
             thai_part = "เอ็ด";
-        elif c == '1' and i == 6:
+            print("i = {}, x = {} เอ็ด 1".format(i, x))
+        elif c == '1' and i == 6 and f > 6 and tmp_number != '0':
             thai_part = "เอ็ด";
+            print("i = {}, x = {} เอ็ด 2".format(i, x))
         elif c == '1' and x == 1:
             thai_part = "";
+            print("i = {}, x = {}".format(i, x))
         elif c == '2' and x == 1:
             thai_part = "ยี่";
+            print("i = {}, x = {} ยี่".format(i, x))
         elif c == '0':
             thai_part = "";
             unit_part = "";
+            print("i = {}, x = {}".format(i, x))
         else:
             thai_part = thai_number[int(c)];
+            print("i = {}, x = {} {}".format(i, x, thai_part))
             
         strnumber = thai_part + unit_part
         result = result + strnumber    
+        tmp_number = c 
         # print("Index {}, number {} {} {}".format(x, c, thai_number[int(c)], unit[x]))
         
     return result
@@ -40,4 +49,4 @@ def coreThai(input_number):
         result = result + "จุด" + toThaiNumber(arr[1])
     return result
     
-print(coreThai('1244523213041.13'))
+print(coreThai('11213011.13'))
